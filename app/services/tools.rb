bfbbeaadcf4cf377146f4c267c471ad98dd7bff7
Setoju <setoju48@gmail.com>
2026-03-02 16:16:35 +0200

    feat: Add tool registry to prevent user from creating own tools

---
+++ app/services/tools.rb
@@ -0,0 +1,26 @@
+module Tools
+  def self.registry
+    @registry ||= {
+      web_search: Tools::WebSearch,
+      news_search: Tools::NewsSearch,
+      weather_search: Tools::WeatherSearch
+    }.freeze
+  end
+
+  def self.fetch(name)
+    registry.fetch(name.to_sym) { raise ArgumentError, "Tool '#{name}' not found." }
+  end
+
+  def self.schema
+    registry.values.map do |tool_class|
+      {
+        type: "function",
+        function: tool_class.schema
+      }
+    end
+  end
+
+  def self.available_tool_names
+    registry.keys.map(&:to_s)
+  end
+end
